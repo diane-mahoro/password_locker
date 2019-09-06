84996d0a565cd457426a7ae49703b4c0857c973f
diane-mahoro <www.diammah7@gmail.com>
2019-09-06 14:55:54 +0200

    if statements

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
 from user_class import User
-from cledential_class import Cledential
+from cledential_class import Cledentials
 def create_account(userName,password):
     '''
     This function creates a new account for a user
@@ -83,6 +83,58 @@ def main():
         mypass=input()
     else:
         print("invalid input")
+    print("Use this short code: ccc- to create nea credential, dc - display credentials, fc - find credentials, ex -exit the credential list")
+    short_code = input().lower()
+    if short_code =='ccc':
+        print("NEW CREDETIAL ACCOUNT")
+        print("_"*10)
+        print("Account Type........")
+        a_type=input()
+        
+        print("User name...........")
+        user_names=input()
+
+        print("Password............")
+        p_word=input()
+        save_cledentials(create_cledential(a_type,user_name,p_word))
+        print('\n')
+
+        print(f"Credential for {a_type} account, User name {user_names} password {p_word}")
+
+        print('\n')
+    elif short_code == 'dc':
+        if display_credentials():
+            print("here is the list of your credentials")
+            print('\n')
+            for x in display_credentials():
+                print(f"{x.type} ...{x.user_name}....{x.password}")
+
+                print('\n')
+        else:
+            print("You don't seem to have any credentials yet")
+            print('\n')
+
+        elif short_code=='fc':
+            print("please enter the type you want to search for")
+
+            search_type=input() 
+            if existing(search_type):
+                search_cledential=find_cledential(search_type)
+                print(f"{search_cledential.user_name}...{search_cledential.password}")
+                print('_'* 20)
+
+                print(f"Account type.......{search_cledential.type}")
+                print(f"User name.......{search_cledential.user_name}")
+                print(f"Password.......{search_cledential.password}")
+
+            else:
+                print("That credential does not exists")  
+        elif short_code== 'ex':
+            print("bye bye .....")
+        else:
+            print("please check properly the short code you are using")
+
+
 if __name__ == '__main__':
 
     main()
