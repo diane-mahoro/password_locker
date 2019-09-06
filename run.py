a59b06f9c5beabae70cafdab4775ab008837b65d
diane-mahoro <www.diammah7@gmail.com>
2019-09-06 10:09:21 +0200

    create function

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3.6
+from user_class import User
+def create_account(userName,password):
+    '''
+    This function creates a new account for a user
+    '''
+    new_user=User(userName,password)
+    return new_user
